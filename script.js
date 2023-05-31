526cb5170db748b1a5ad58c9937c1702a4c0c59e
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-05-31 18:38:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ function decimalToBinary(num) {
     return res.reverse().join('');
 }
 
-module.exports = decimalToBinary;
-
 console.log(decimalToBinary(7)); // Output: 111
 console.log(decimalToBinary(10)); // Output: 1010
 console.log(decimalToBinary(33)); // Output: 100001
+module.exports = decimalToBinary;
+
+
