c3e475656375d33efc45872438cf8368f23952fb
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-05-31 18:26:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function threeSum(arr, target) {
+function decimalToBinary(num) {
 // write your code here
-  
+	while(num>0){
+		let  res=[];
+	  res= num%2;
+	num=num/2;
+	}
+	Array.prototype.reverse(res);
 }
 
-module.exports = threeSum;
+module.exports = decimalToBinary;
