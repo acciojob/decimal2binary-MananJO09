1d95a46d3c7f3940973c93e7cfd783dee7fcaea9
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-07-03 17:45:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,11 +4,8 @@
       while (num > 0) {
         let rem = num % 2;
         binaryStr = binaryStr + rem.toString();
-     
         num = Math.floor(num / 2);
       }
       return binaryStr.split("").reverse().join("");
     }
-     
     window.decimalToBinary = decimalToBinary;
-
